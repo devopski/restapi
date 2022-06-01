6aef3b17169c4cc1948201bb545839bde956643e
Dawid <jedrzejczakdaw@gmail.com>
2022-06-01 10:55:45 +0200

    Initial

---
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -0,0 +1,20 @@
+package com.example.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String Hello(){
+		return "Hello from Java app!";
+	}
+}
